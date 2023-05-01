78532a58448f428a7514ef39171df8612cc1246f
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2023-05-01 11:45:22 -0300

    second commit

--- src/external/database/chucknorris_repository.rs
+++ src/external/database/chucknorris_repository.rs
@@ -1,4 +1,4 @@
-use rusqlite::{Connection, Error, Result};
+use rusqlite::{Connection, Result};
 
 #[derive(Debug)]
 pub struct ChuckNorris {
@@ -15,10 +15,38 @@ fn setup() -> Result<()> {
 
     conn.execute(
         "CREATE TABLE chucknorris (
-            id    INTEGER PRIMARY KEY,
-            name  TEXT NOT NULL,
-            data  BLOB
+            id    TEXT PRIMARY KEY,
+            icon_url  TEXT NOT NULL,
+            url  TEXT NOT NULL,
+            created_at  TEXT NOT NULL,
+            updated_at  TEXT NOT NULL
         )",
         (), // empty list of parameters.
     )?;
+
+    Ok(())
+}
+
+fn all() -> Result<Vec<ChuckNorris>> {
+    let conn = Connection::open_in_memory()?;
+
+    let mut stmt = conn.prepare("SELECT id, icon_url, url, created_at, updated_at FROM chucknorris")?;
+    let chuck_norris_iter = stmt.query_map([], |row| {
+        Ok(ChuckNorris {
+            categories: vec![],
+            id: row.get(0)?,
+            icon_url: row.get(1)?,
+            url: row.get(2)?,
+            created_at: row.get(3)?,
+            updated_at: row.get(4)?,
+        })
+    })?;
+
+    let mut chucks = Vec::new();
+
+    for chuck in chuck_norris_iter {
+        chucks.push(chuck.unwrap());
+    }
+
+    Ok(chucks)
 }
