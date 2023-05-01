e8fd526bb79194d2230dac6206a0ee37bb709d4b
Gabriel Cardoso Luiz <gabriel.cardoso010@gmail.com>
2023-05-01 16:14:34 -0300

    feat: added insert and get_by_id function

--- src/external/database/chucknorris_repository.rs
+++ src/external/database/chucknorris_repository.rs
@@ -10,8 +10,12 @@ pub struct ChuckNorris {
     url: String,
 }
 
-fn setup() -> Result<()> {
-    let conn = Connection::open_in_memory()?;
+fn get_connection() -> Result<Connection> {
+    Connection::open_in_memory()
+}
+
+pub fn setup() -> Result<()> {
+    let conn = get_connection()?;
 
     conn.execute(
         "CREATE TABLE chucknorris (
@@ -27,18 +31,18 @@ fn setup() -> Result<()> {
     Ok(())
 }
 
-fn all() -> Result<Vec<ChuckNorris>> {
-    let conn = Connection::open_in_memory()?;
+pub fn all() -> Result<Vec<ChuckNorris>> {
+    let conn = get_connection()?;
 
     let mut stmt = conn.prepare("SELECT id, icon_url, url, created_at, updated_at FROM chucknorris")?;
     let chuck_norris_iter = stmt.query_map([], |row| {
         Ok(ChuckNorris {
-            categories: vec![],
             id: row.get(0)?,
             icon_url: row.get(1)?,
             url: row.get(2)?,
             created_at: row.get(3)?,
             updated_at: row.get(4)?,
+            categories: vec![],
         })
     })?;
 
@@ -50,3 +54,38 @@ fn all() -> Result<Vec<ChuckNorris>> {
 
     Ok(chucks)
 }
+
+pub fn get_by_id(id: i32) -> Result<Vec<ChuckNorris>> {
+    let conn = get_connection()?;
+
+    let mut stmt = conn.prepare("SELECT id, icon_url, url, created_at, updated_at FROM chucknorris where id = :id")?;
+    let chuck_norris_iter = stmt.query_map(&[(":id", &id)], |row| {
+        Ok(ChuckNorris {
+            id: row.get(0)?,
+            icon_url: row.get(1)?,
+            url: row.get(2)?,
+            created_at: row.get(3)?,
+            updated_at: row.get(4)?,
+            categories: vec![],
+        })
+    })?;
+
+    let mut chucks = Vec::new();
+
+    for chuck in chuck_norris_iter {
+        chucks.push(chuck.unwrap());
+    }
+
+    Ok(chucks)
+}
+
+pub fn insert(chuck: ChuckNorris) -> Result<usize> {
+    let conn = get_connection()?;
+
+    conn.execute(
+        "INSERT INTO person (icon_url, url, created_at, updated_at) VALUES (?1, ?2)",
+        (&chuck.icon_url, &chuck.url, &chuck.created_at, &chuck.updated_at),
+    )
+}
+
+
